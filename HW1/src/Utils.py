e11d1b6f7c5ea9f46dc5b2267fb06b4c5cb72381
insamuel <i_samuelsson@yahoo.com>
2023-02-02 14:11:23 -0500

    fixed config file for running tests due to reorganizeing file structure

--- HW1/src/Utils.py
+++ HW1/src/Utils.py
@@ -1,10 +1,14 @@
 from subprocess import call
-import yaml
 import math
-import random
+import yaml
+from pathlib import Path
 
-with open("config.yml", "r") as config_file:
+my_path = Path(__file__).resolve()  # resolve to get rid of any symlinks
+config_path = my_path.parent / 'config.yml'
+
+with config_path.open() as config_file:
     configs = yaml.safe_load(config_file)
+
 
 passer = ""
 
