1425fe3f7359608cbb82f89c17ddbc0f1dc8c458
Rithik Jain <rjain25@ncsu.edu>
2023-02-16 19:10:09 -0500

    Added config, Test and Util files from HW-4

---
+++ src/HW5/Tests.py
@@ -0,0 +1,293 @@
+import sys
+import TestEngine
+import Common
+import Repgrid
+
+from Data import Data
+from Num import Num
+from Sym import Sym
+from Row import Row
+from Cols import Cols
+from Utils import rnd, canPrint, rand, set_seed, read_csv
+
+command_line_args = []
+
+
+
+##
+# Imports sys, TestEngine, Common, Num from Num, Sym from Sym, rnd,
+# canPrint, rand, set_seed, csv from Utils
+#
+# Checks if it's possible to print the value of the "the" field in the
+# "cfg" dictionary in the "Common" module.
+#
+# Call the "canPrint" function with the "Common.cfg['the']" argument and
+# the string "Should be able to print the". If the "canPrint" function
+# executes without raising an exception, the "eg_the" function returns
+# True, which represents that the test passed.
+##
+@TestEngine.test
+def eg_the():
+    canPrint(Common.cfg['the'], 'Should be able to print the')
+    return True
+
+##
+# Defines a test function named "eg_rand" using the @TestEngine.test
+# decorator.
+#
+# Checks if two sets of random numbers generated with the same seed are
+# equal, and if the midpoint of the set of random numbers is equal to 0.5.
+#
+# It creates two instances of the Num class, "num1" and "num2".
+#
+# It sets a seed for the random number generator using the set_seed
+# function, with a value of 937162211. It uses a for loop to add 1000
+# random numbers in the range of 0 to 1 to "num1".
+#
+# It sets the same seed for the random number generator again. It uses
+# another for loop to add 1000 random numbers in the range of 0 to 1 to
+# "num2".
+#
+# It calculates the midpoints of the "num1" and "num2" instances, rounded
+# to 10 decimal places, and stores them in "m1" and "m2". It formats a
+# string with "m1", "m2", and the rounded value of "m1". It calls the
+# canPrint function to print the formatted string along with the message
+# "M1 and M2 should be equal".
+##
+@TestEngine.test
+def eg_rand():
+    num1, num2 = Num(), Num()
+
+    set_seed(937162211)
+    for i in range(1, 10 ** 3 + 1):
+        x = rand(0, 1)
+        num1.add(x)
+
+    set_seed(937162211)
+    for i in range(1, 10 ** 3 + 1):
+        num2.add(rand(0, 1))
+
+    m1, m2 = round(num1.mid(), 10), round(num2.mid(), 10)
+    results = "m1= {}, m2= {}, rounded= {}".format(m1, m2, round(m1, 1))
+    canPrint(results, 'M1 and M2 should be equal')
+    return m1 == m2 and .5 == round(m1, 1)
+
+##
+# Defines a test function named eg_sym using the @TestEngine.test
+# decorator.
+#
+# Returns the result of the comparison between the calculated
+# midpoint and 11/7 and the calculated variance and 0.787.
+#
+# The function creates an instance of the Sym class and adds
+# some string values to it.
+#
+# The function then calculates the mode and entropy of the symbols and
+# rounds the entropy value to 3 decimal places. It then prints the mode and
+# entropy values in a formatted string.
+#
+# Returns true if the mode is "a" and the entropy value is 1.379.
+##
+@TestEngine.test
+def eg_sym():
+    s = Sym()
+
+    test_vals = ["a", "a", "a", "a", "b", "b", "c"]
+
+    for x in test_vals:
+        s.add(x)
+
+    # mode, entropy = s.mid(), rnd(s.div(), 3)
+    # results = "mid= {}, div= {}".format(mode, entropy)
+    # canPrint(results, 'Should be able to print mid and div')
+
+    res = ('a' == s.mid()) and (1.379 == rnd(s.div(), 3))
+    return res
+
+##
+# Defines a test function named eg_num using the @TestEngine.test
+# decorator.
+#
+# Checks the Num class. It creates an instance of the Num class named n.
+#
+# It adds the elements of the list test_vals to the instance of Num using
+# the add method.
+#
+# It calculates the midpoint (mean) and the variance of the elements added
+# to the Num instance.
+#
+# It formats the midpoint, div values and the string "Should be
+# able to print mid and div" into a single string and passes it to the
+# function canPrint.
+##
+@TestEngine.test
+def eg_num():
+    n = Num()
+    test_vals = [1,1,1,1,2,2,3]
+    for x in test_vals:
+        n.add(x)
+
+    mid, div = n.mid(), round(n.div(), 3)
+    results = "mid= {}, div= {}".format(mid, div)
+    canPrint(results, 'Should be able to print mid and div')
+
+    return 11/7 == mid and 0.787 == div
+
+@TestEngine.test
+def eg_csv():
+    row_count = 0
+    def line_handler(xs: Row):
+        nonlocal row_count
+        row_count += 1
+    read_csv(Common.cfg["the"]["file"], line_handler)
+    return row_count == 399
+
+@TestEngine.test
+def eg_duplicate_structure():
+    d1 = Data(Common.cfg['the']['file'])
+    d2 = d1.clone()
+    return len(d1.rows) == len(d2.rows) and d1.cols.y[1].w == d2.cols.y[1].w and d1.cols.y[1].at == d2.cols.y[1].at
+
+@TestEngine.test 
+def test_data():
+    # i know this is horrible but it works
+    expected_output = '\ny\tmid\t{ :Lbs- 2970.42 :Acc+ 15.57 :Mpg+ 23.84}\n \tdiv\t{ :Lbs- 846.84 :Acc+ 2.76 :Mpg+ 8.34}\nx\tmid\t{ :Clndrs 5.45 :Volume 193.43 :Model 76.01 :origin 1}\n \tdiv\t{ :Clndrs 1.7 :Volume 104.27 :Model 3.7 :origin 1.3273558482394003}'
+    test_data = Data('../../etc/data/auto93.csv')
+    
+    y_mid_report = '{'
+    y_div_report = '{'
+    for y in test_data.cols.y:
+        y_mid_report = y_mid_report + ' :' + y.txt + ' ' + str(y.rnd(y.mid(), 2))
+        y_div_report = y_div_report + ' :' + y.txt + ' ' + str(y.rnd(y.div(), 2))
+    y_mid_report = y_mid_report + '}'
+    y_div_report = y_div_report + '}'
+
+    x_mid_report = '{'
+    x_div_report = '{'
+    for x in test_data.cols.x:
+        x_mid_report = x_mid_report + ' :' + x.txt + ' ' + str(x.rnd(x.mid(), 2))
+        x_div_report = x_div_report + ' :' + x.txt + ' ' + str(x.rnd(x.div(), 2))
+    x_mid_report = x_mid_report + '}'
+    x_div_report = x_div_report + '}'
+
+    res_string = '\ny\tmid\t' + y_mid_report + '\n \tdiv\t' + y_div_report + '\nx\tmid\t' + x_mid_report + '\n \tdiv\t' + x_div_report
+    print(res_string)
+    return res_string == expected_output
+
+#@TestEngine.test
+def test_sort_nearest_neighbor():
+    data = Data('../../etc/data/auto93.csv')
+    around_res = data.around(data.rows[0])
+
+    expected_output = "0\t0.0\t['8', '304.0', '193', '4732', '18.5', '70', '1', '10']\n50\t0.17\t['8', '318', '150', '4457', '13.5', '74', '1', '10']\n100\t0.28\t['6', '232', '100', '2901', '16', '74', '1', '20']\n150\t0.38\t['8', '351', '142', '4054', '14.3', '79', '1', '20']\n200\t0.55\t['4', '122', '96', '2300', '15.5', '77', '1', '30']\n250\t0.68\t['4', '119', '82', '2720', '19.4', '82', '1', '30']\n300\t0.71\t['4', '119', '97', '2545', '17', '75', '3', '20']\n350\t0.79\t['4', '146', '67', '3250', '21.8', '80', '2', '30']\n"
+    res_string = ""
+    for i in range(len(around_res)):
+        if i % 50 == 0:
+            row = around_res[max(0, i -1)]
+            dist = rnd(data.dist(row, data.rows[0]), 2)
+            res_string+= str(i) + "\t" + str(dist) + '\t' + str(row.cells) + '\n'
+    print(res_string)
+    return expected_output == res_string
+
+#@TestEngine.test
+def test_one_level_bi_clustering():
+    data = Data('../../etc/data/auto93.csv')
+    set_seed(937162211)
+    half_res = data.half()
+    res_string = str(len(half_res['left'])) + '\t' + str(len(half_res['right'])) + '\t' + str(len(data.rows)) + '\n'
+    res_string += str(half_res['A'].cells) + '\t' + str(half_res['c']) + '\n'
+    res_string+= str(half_res['mid'].cells) + '\n'
+    res_string+= str(half_res['B'].cells) + '\n'
+    print(res_string)
+
+    #TODO this answer *seems* correct. we use a different random number generator so our A and B values are different. 
+    # will probably need more confirmation as the project goes on
+    expected_res = "199\t199\t398\n['4', '90', '48', '1985', '21.5', '78', '2', '40']\t0.6393183441778267\n['4', '151', '90', '2950', '17.3', '82', '1', '30']\n['6', '232', '100', '2634', '13', '71', '1', '20']\n"
+    return res_string == expected_res
+
+def show_hw3(cluster_res, cols, n_places, level):
+    if cluster_res != None:
+        report_string = ('| '*level)
+
+        data = cluster_res['data']
+
+        report_string+= ' ' + str(len(data.rows))
+        if 'left' not in cluster_res or level == 0:
+            report_string+= ' { '
+            for y in data.cols.y:
+                report_string+= ' :' + y.txt + ' ' + str(y.rnd(y.mid(), n_places))
+            report_string+= ' }'
+        print(report_string)
+
+        show(cluster_res['left'] if 'left' in cluster_res else None, cols, n_places, level + 1)
+        show(cluster_res['right'] if 'right' in cluster_res else None, cols, n_places, level + 1)
+
+def show_hw4(cluster_res, cols, n_places, level):
+    if cluster_res != None:
+        report_string = ('| '*level)
+
+        data = cluster_res['data']
+
+        report_string+= ' ' + str(len(data.rows))
+        if 'left' not in cluster_res:
+            report_string+= ' { ' + str(data.rows[-1].cells[-1])
+        else:
+            report_string+= ' { ' + str(rnd(100 * cluster_res['c']))
+        report_string+= ' }'
+        print(report_string)
+
+        show_hw4(cluster_res['left'] if 'left' in cluster_res else None, cols, n_places, level + 1)
+        show_hw4(cluster_res['right'] if 'right' in cluster_res else None, cols, n_places, level + 1)
+
+
+#@TestEngine.test
+def test_cluster():
+    data = Data('../../etc/data/auto93.csv')
+
+    show_hw3(data.cluster(), data.cols.y, 1, 0)
+
+    #TODO check if this is actually working (randomness changes output from what dr. menzies has)
+    return True
+
+#@TestEngine.test
+def test_sway():
+    data = Data('../../etc/data/auto93.csv')
+    show_hw3(data.sway(), data.cols.y, 1, 0)
+
+    #TODO check if this is actually working (randomness changes output from what dr. menzies has)
+    return True
+
+@TestEngine.test
+def test_repcols():
+    generated_data = Repgrid.process_repgrid_file('../../etc/data/repgrid1.csv')
+    show_hw4(generated_data.cluster(), generated_data.cols.y, 1, 0)
+
+    #TODO check if this is actually working (randomness changes output from what dr. menzies has)
+    return True
+
+##
+# Defines a function ALL using @TestEngine.test. This function calls other
+# functions, whose names start with eg_, stored in Common.eg, one by one,
+# and prints their results. The function also keeps track of the number of
+# failed tests in the Common.fails variable. The function returns True at
+# the end.
+##
+@TestEngine.test
+def ALL():
+    for k in Common.eg:
+        if k != "ALL":
+            print('\n' + "---------------------------------------")
+            if not TestEngine.runs(k):
+                Common.fails += 1
+    return True
+
+##
+# Checks if the script is being run as the main program and if so, it calls
+# the TestEngine.runs function with the value of the eg key of the
+# dictionary of Common.cfg as its argument. After the call to TestEngine
+# runs, the program exits with the value of Common.fails as the exit status
+# code.
+##
+if __name__ == "__main__":
+    TestEngine.runs(Common.cfg["the"]["eg"])
+    sys.exit(Common.fails)
---
+++ src/HW5/Utils.py
@@ -0,0 +1,287 @@
+from subprocess import call
+import os
+import csv
+import random
+
+import Common
+import math
+import yaml
+from pathlib import Path
+
+##
+# Imports call from subprocess, math, yaml and Path from pathlib
+#
+# Provides several functions and a class for test purposes. Includes
+# functions for reading values from a YAML config file, generating
+# pseudo-random numbers, type-casting strings to appropriate data types,
+# reading CSV files, processing command-line arguments, etc. It also
+# defines a custom exception class named "TestError" for raising exceptions
+# in case of errors during tests.
+##
+
+##
+# Retrieves the parent directory path of the current file and appends
+# "config.yml" to it to create a config_path variable.
+##
+my_path = Path(__file__).resolve()  # resolve to get rid of any symlinks
+config_path = my_path.parent / 'config.yml'
+
+##
+# Opens the file at that path and uses the yaml.safe_load method to parse
+# the YAML data in the file into a Python object configs. passer is
+# assigned an empty string value.
+##
+with config_path.open() as config_file:
+    configs = yaml.safe_load(config_file)
+
+
+passer = ""
+
+##
+# Defines a custom exception class named "TestError". The class has an
+# __init__ method that takes a string argument mess and assigns it to an
+# instance variable self.mess. This custom exception can be raised and
+# caught in a try-except block to handle specific error conditions in a
+# program.
+##
+class TestError(Exception):
+    def __init__(self, mess):
+        self.mess = mess
+
+##
+# Defines a function "canPrint" which takes two arguments "content" and
+# "mess". The function first tries to print the "content". If there is an
+# exception raised while trying to print "content", it will raise a custom
+# exception "TestError" with the message specified in the argument "mess".
+##
+def canPrint(content, mess):
+    try:
+        print(content)
+    except:
+        raise TestError(mess)
+
+##
+# Takes a string as an argument and returns a Boolean indicating whether
+# the string can be converted to a floating-point number or not. If the
+# string can be converted, the function returns True. If not, the function
+# returns False.
+##
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+##
+# Defines a function "rand" that generates a pseudo-random number within a
+# specified range (lo and hi). The number is generated based on a given
+# seed value stored in the "configs" dictionary, which is multiplied by
+# 16807 and then taken modulo 2147483647 to get the next seed. The
+# generated number is then calculated by adding "lo" to the result of (hi -
+# lo) multiplied by the seed value divided by 2147483647.
+##
+def rand(lo = None, hi = None):
+    """
+    Generates a pseudo-random number using seed.
+    :param lo: Lower limit of generated number
+    :param hi: Higher limit of generated number
+    :return: Pseudo-random number
+    """
+    if lo is None:
+        lo = 0
+
+    if hi is None:
+        hi = 1
+
+    rand = random.Random()
+    rand.seed(Common.cfg['the']['seed'])
+    return rand.randrange(lo, hi)
+
+def get_rand_items(list, item_count: int):
+    return random.choices(list, k=item_count)
+
+##
+# Defines a function "rnd" that takes a floating point number x and an
+# argument places, which sets the number of decimal places for the rounded
+# result.
+#
+# It calculates the rounded value by multiplying x with 10 raised to the
+# power of places, adding 0.5 to it. Then, rounding down to the nearest
+# integer (floor), and then dividing it by the same value of 10 raised to
+# the power of places. The rounded value is returned as the final result.
+##
+def rnd(x, places=2):
+    mult = 10 ** places
+    return math.floor(x * mult + 0.5) / mult
+
+##
+# Defines a function coerce that takes a string input s and converts it to
+# its equivalent data type (either integer, boolean or string).
+#
+# The function coerce first checks if the input string s is a number by
+# calling the function is_number. If s is a number, the function coerce
+# returns a float representation of s. If s is not a number, the function
+# returns None or the result of function fun with s as the argument.
+##
+def coerce(s):
+    ##
+    # Convert input in the form of a string to appropriate data type int
+    # bool/str
+    #
+    # The function fun takes a string s1 as input and returns a
+    # boolean value if the string is equal to "true" or "false". If s1 is
+    # neither of those values, fun returns s1.
+    ##
+    def fun(s1):
+        # returns boolean for the string
+        if s == "true":
+            return True
+        elif s == "false":
+            return False
+        return s1
+
+    ##
+    # Return integer of the number in the form of string or calls fun to
+    # return appropriately
+    ##
+    if is_number(s):
+        return float(s)
+    else:
+        return None or fun(s)
+
+#find x,y from a line connecting `a` to `b`
+def cos(a, b, c):
+    c = max(c, 1)
+    x1 = (pow(a, 2) + pow(b, 2) + pow(c, 2)) / (2 * c)
+    x2 = max(0, min(1, x1))
+    y = pow((pow(a, 2) - pow(x2, 2)), 0.5)
+    return {'x': x2, 'y': y}
+
+
+##
+# Call "fun" on each row. Row cells are divided in "the.seperator"
+#
+# Defines a function csv(fname, fun=None), which reads a CSV (Comma
+# Separated Values) file specified by fname, typecasts every cell in each
+# row to appropriate data type (int, bool or str) using the coerce()
+# function, and applies the function fun to every row of the CSV file, if
+# fun is specified.
+#
+# If fname is None or an empty string, an Exception is raised with message
+# "File not found". The separator used to split a row is specified in
+# Common.cfg['the']['separator']. The new line (\n) at the end of each row
+# is removed before typecasting the cells.
+##
+def read_csv(fname, fun=None):
+    if fname is None or len(fname.strip()) == 0:
+        raise Exception("File not found")
+    else:
+
+        #try to catch relative paths
+        if not os.path.isfile(fname):
+            fname = os.path.join(os.path.dirname(__file__), fname)
+
+        fname = os.path.abspath(fname)
+
+        csv_list = []
+        with open(fname, 'r') as csv_file:
+            csv_list = list(csv.reader(csv_file, delimiter=','))
+        
+        if fun != None:
+            for item in csv_list:
+                fun(item)
+
+##
+# Function that processes command line arguments passed to the script. The
+# function takes two arguments: args: A string of command line arguments
+# separated by a space and configs: A dictionary that stores the
+# configuration of the script.
+#
+# The function returns the updated configs dictionary
+##
+def cli(args, configs):
+    arg_arr = args.split(" ")
+
+
+    if '-e' in arg_arr:
+        arg_arr.remove("-e")
+
+
+    def find_arg_value(args: list[str], optionA: str, optionB: str) -> str:
+        index = args.index(optionA) if optionA in args else args.index(optionB)
+        if (index + 1) < len(args):
+            return args[index + 1]
+        return None
+
+    configs['the']['help'] = '-h' in args or '--help' in args
+    configs['the']['go'] = '-g' in args or '--go' in args
+    configs['the']['quit'] = '-q' in args or '--quit' in args
+    configs['the']['dump'] = '-d' in args or '--dump' in args
+    configs['the']['file'] = find_arg_value(args, '-f', '--file') if ('-f' in args or '--file' in args) else '../../etc/data/auto93.csv'
+   
+    #find the seed value
+    if '-s' in args or '--seed' in args:
+        seed_value = find_arg_value(args, '-s', '--seed')
+        if seed_value is not None:
+            try:
+                configs['the']['seed'] = int(seed_value)
+            except ValueError:
+                raise ValueError("Seed value must be an integer!")
+    else:
+        configs['the']['seed'] = 937162211
+
+    #find the far value
+    if '-f' in args or '--far' in args:
+        far_value = find_arg_value(args, '-f', '--far')
+        if far_value is not None:
+            try:
+                configs['the']['far'] = float(far_value)
+            except ValueError:
+                raise ValueError("Far value must be a float!")
+    else:
+        configs['the']['far'] = 0.95
+
+    #find the min value
+    if '-m' in args or '--min' in args:
+        min_value = find_arg_value(args, '-m', '--min')
+        if min_value is not None:
+            try:
+                configs['the']['min'] = float(min_value)
+            except ValueError:
+                raise ValueError("Min value must be a float!")
+    else:
+        configs['the']['min'] = 0.5
+
+    #find the p value
+    if '-p' in args or '--p' in args:
+        p_value = find_arg_value(args, '-p', '--p')
+        if p_value is not None:
+            try:
+                configs['the']['p'] = int(p_value)
+            except ValueError:
+                raise ValueError("P value must be an integer!")
+    else:
+        configs['the']['p'] = 2
+    
+    #find the sample value
+    if '-s' in args or '--sample' in args:
+        sample_value = find_arg_value(args, '-s', '--sample')
+        if sample_value is not None:
+            try:
+                configs['the']['sample'] = int(sample_value)
+            except ValueError:
+                raise ValueError("Sample value must be an integer!")
+    else:
+        configs['the']['sample'] = 512
+
+    return configs
+
+##
+# Function sets the value of seed in the dictionary configs['the'] to x.
+# The configs dictionary holds the configuration data and the seed key in
+# it holds the value for a seed used for generating random numbers. This
+# function sets the value of seed to x.
+##
+def set_seed(x):
+    configs['the']['seed'] = x
