33577ff0c68dd9f2f179d6e571559fa3f07e4095
Rithik Jain <rjain25@ncsu.edu>
2023-03-21 18:55:23 -0400

    Added erf function in Utils.py for HW-7

--- src/HW7/Utils.py
+++ src/HW7/Utils.py
@@ -390,3 +390,32 @@ def prune(rule, maxSize):
             rule[txt] = None
     if n > 0:
         return rule
+
+    ##
+    # Calculates an approximation of the error function, also known as the Gauss error function, for a
+    # given input x
+    ##
+    def erf(x):
+        # from Abramowitz and Stegun 7.1.26
+        # https://s3.amazonaws.com/nrbook.com/AandS-a4-v1-2.pdf
+        # (easier to read at https://en.wikipedia.org/wiki/Error_function#Approximation_with_elementary_functions)
+        a1  = 0.254829592
+        a2  = -0.284496736
+        a3  = 1.421413741
+        a4  = -1.453152027
+        a5  = 1.061405429
+        p   = 0.3275911
+
+        # Save the sign of x
+        sign = 1
+
+        if x < 0:
+            sign = -1
+
+        x = math.abs(x)
+
+        # A&S formula 7.1.26
+        t = 1.0 / (1.0 + p*x)
+        y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*math.exp(-x*x)
+
+        return sign*y
